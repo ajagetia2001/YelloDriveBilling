a767d8b557cc3dbf675dcc19a89c5596e677e098
ajagetia2001 <jagetiaaaryan06@gmail.com>
2022-08-29 11:03:01 +0530

    added whatsapp post request to send a message

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,50 @@
+const fs = require("fs");
+const qrcode = require("qrcode-terminal");
 const express = require("express");
+const cors = require("cors");
+const { Client, MessageMedia, LocalAuth } = require("whatsapp-web.js");
 const app = express();
-const port = 5000;
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const sendWithApi = (req, res) => {
+  const { message, to } = req.body;
+  const newNumber = `91${to}@c.us`;
+  console.log(message, to);
+  sendMessage(newNumber, message);
+  res.send({ status: "success" });
+};
+app.post("/send", sendWithApi);
+const client = new Client({
+  authStrategy: new LocalAuth()
+});
+client.on("qr", qr => {
+  qrcode.generate(qr, { small: true });
+});
+client.on("authenticated", session => {
+  console.log("auth");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+client.on("ready", () => {
+  console.log("Client is ready!");
 });
+// client.on("message", message => {
+//   console.log(message.body);
+// });
+function listenMessage() {
+  client.on("message", msg => {
+    const { from, to, body } = msg;
+    console.log(from, to, body);
+    sendMessage(from, "sads");
+  });
+}
+const sendMessage = (to, message) => {
+  client.sendMessage(to, message);
+};
+const sendMedia = (to, file) => {
+  const mediafile = MessageMedia.fromFilePath(`./media/${file}`);
+  client.sendMessage(to, mediafile);
+};
+
+client.initialize();
+app.listen(5000, () => {});
